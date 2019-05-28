e15000f2b1167274cc89a804511fbec0107b84cc
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-05-28 10:38:01 +0200

    feat: Introduce bundle analyzer.

---
+++ packages/the-react-scripts/scripts/analyze.js
@@ -0,0 +1,15 @@
+process.env.NODE_ENV = 'production';
+
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpackConfig = require('../internals/webpack/webpack.prod');
+
+// pushing BundleAnalyzerPlugin to plugins array
+webpackConfig.plugins.push(new BundleAnalyzerPlugin());
+
+// actually running compilation and waiting for plugin to start explorer
+webpack(webpackConfig, (err, stats) => {
+  if (err || stats.hasErrors()) {
+    console.error(err);
+  }
+});
