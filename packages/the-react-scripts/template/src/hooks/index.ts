d0c256d9cb4a8e8a3ab33389e26fa4fab45b899b
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-06-01 11:39:59 +0200

    refactor(AppProvider): Use theme context more effectively.

--- packages/the-react-scripts/template/src/hooks/index.ts
+++ packages/the-react-scripts/template/src/hooks/index.ts
@@ -5,4 +5,5 @@ export { default as useMedia } from './useMedia';
 export { default as useNetworkStatus } from './useNetworkStatus';
 export { default as usePreviousLocation } from './usePreviousLocation';
 export { default as useRouter } from './useRouter';
+export { default as useTheme } from './useTheme';
 export { default as useWindowSize } from './useWindowSize';
