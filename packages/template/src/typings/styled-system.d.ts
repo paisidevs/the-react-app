fe2511d01e96e5315390efc25beea8023565a424
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-11-13 18:11:47 +0200

    other: return chalk package to v2.4.2

--- packages/template/src/typings/styled-system.d.ts
+++ packages/template/src/typings/styled-system.d.ts
@@ -1,32 +1,4 @@
 import 'styled-system';
-import {
-  AlignItemsProps,
-  BorderProps,
-  BorderRadiusProps,
-  BordersProps,
-  BoxShadowProps,
-  ColorProps,
-  DisplayProps,
-  FlexDirectionProps,
-  FlexProps,
-  FlexWrapProps,
-  FontFamilyProps,
-  FontSizeProps,
-  FontWeightProps,
-  HeightProps,
-  JustifyContentProps,
-  MaxHeightProps,
-  MaxWidthProps,
-  MinHeightProps,
-  MinWidthProps,
-  OverflowProps,
-  PositionProps,
-  SizeProps,
-  SpaceProps,
-  TextAlignProps,
-  WidthProps,
-  ZIndexProps,
-} from 'styled-system';
 
 declare module 'styled-system' {
   export interface StyledSystemProps
@@ -55,5 +27,7 @@ declare module 'styled-system' {
       SpaceProps,
       TextAlignProps,
       WidthProps,
-      ZIndexProps {}
+      ZIndexProps {
+    color?: string | (string & string[]);
+  }
 }
