a6cf412f87e74eb7b2a13a85f2514d6d5c2f151b
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2019-12-27 20:42:59 +0200

    feat: setup apollo client

--- packages/the-app/src/contexts/AuthenticationProvider.context.tsx
+++ packages/the-app/src/contexts/AuthenticationProvider.context.tsx
@@ -1,5 +1,27 @@
 import React, { FC, useState } from 'react';
 
+type CognitoUser = {
+  id: string;
+  accessToken: string;
+  attributes: {
+    email: string;
+    name: string;
+    role: 'admin' | 'subscriber';
+    username?: string;
+  };
+};
+
+const MOCK_COGNITO_USER: CognitoUser = {
+  id: 'abcd1234',
+  accessToken: 'abcd1234-ef56',
+  attributes: {
+    email: 'mpofuthandolwethu@gmail.com',
+    name: 'Thandolwethu Mpofu',
+    username: 'elandamor',
+    role: 'admin' as 'admin',
+  },
+};
+
 const Errors = {
   accountExists: {
     message: 'An account with this email already exists',
@@ -10,23 +32,29 @@ const DEFAULT_STATE = {
   Errors,
   authenticating: true,
   isAuthenticated: true,
+  currentAuthenticatedUser: () => {},
   signIn: (_email: string, _password: string) => {},
   signUp: (_email: string, _password: string) => {},
 };
 
 export const AuthenticationContext = React.createContext(DEFAULT_STATE);
 
-interface IProps {
-  children: React.ReactNode;
-}
-
-const Provider: FC<IProps> = ({ children }) => {
+const Provider: FC<{}> = ({ children }) => {
   const [authenticating] = useState(DEFAULT_STATE.authenticating);
   const [isAuthenticated, setIsAuthenticated] = useState(
     DEFAULT_STATE.isAuthenticated,
   );
 
-  const signUp = (email: string) =>
+  const currentAuthenticatedUser = (): Promise<CognitoUser> =>
+    new Promise((resolve, reject) => {
+      try {
+        resolve(MOCK_COGNITO_USER);
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+  const signUp = (email: string): Promise<CognitoUser> =>
     new Promise((resolve, reject) => {
       try {
         setTimeout(() => {
@@ -34,18 +62,18 @@ const Provider: FC<IProps> = ({ children }) => {
             reject(new Error(Errors.accountExists.message));
           }
 
-          resolve({ data: { user: { name: 'Thandolwethu Mpofu' } } });
+          resolve(MOCK_COGNITO_USER);
         }, 3000);
       } catch (error) {
         reject(error);
       }
     });
 
-  const signIn = () =>
+  const signIn = (): Promise<CognitoUser> =>
     new Promise((resolve, reject) => {
       try {
         setTimeout(() => {
-          resolve({ data: { user: { name: 'Thandolwethu Mpofu' } } });
+          resolve(MOCK_COGNITO_USER);
 
           setTimeout(() => {
             setIsAuthenticated(true);
@@ -58,7 +86,14 @@ const Provider: FC<IProps> = ({ children }) => {
 
   return (
     <AuthenticationContext.Provider
-      value={{ authenticating, Errors, isAuthenticated, signIn, signUp }}
+      value={{
+        authenticating,
+        currentAuthenticatedUser,
+        Errors,
+        isAuthenticated,
+        signIn,
+        signUp,
+      }}
     >
       {children}
     </AuthenticationContext.Provider>
---
+++ packages/the-app/src/utilities/apollo-utilities.ts
@@ -0,0 +1,120 @@
+import { ApolloLink, NextLink, Operation } from 'apollo-link';
+import { onError } from 'apollo-link-error';
+
+const formatError = (message: any, path?: any) => {
+  if (path) {
+    const headerCss = [
+      'color: gray; font-weight: lighter', // title
+      'color: #333; font-weight: 600', // message
+      'color: gray; font-weight: lighter', // on
+      'color: #333; font-weight: 600', // path
+      'color: gray; font-weight: lighter', // request.
+    ];
+
+    const parts = ['%c message'];
+
+    parts.push(`%c${message}`);
+    parts.push('%con');
+    parts.push(`%c${path.join('.')}`);
+    parts.push('%crequest');
+
+    return [parts.join(' '), ...headerCss];
+  }
+
+  const headerCss = [
+    'color: gray; font-weight: lighter', // title
+    'color: #333; font-weight: 600', // message
+  ];
+
+  const parts = ['%c message'];
+
+  parts.push(`%c${message}`);
+
+  return [parts.join(' '), ...headerCss];
+};
+
+const formatMessage = (
+  operationType: string,
+  operation: any,
+  elapsed?: any,
+) => {
+  const headerCss = [
+    'color: gray; font-weight: lighter', // title
+    `color: ${operationType === 'query' ? '#02B875' : '#03A9F4'};`, // operationType
+    'color: #000;', // operationName
+  ];
+
+  const parts = ['%c apollo'];
+
+  parts.push(`%c${operationType}`);
+  parts.push(`%c${operation.operationName}`);
+
+  if (elapsed) {
+    parts.push(`%c(in ${elapsed} ms)`);
+    headerCss.push('color: gray; font-weight: lighter;'); // time
+  }
+
+  return [parts.join(' '), ...headerCss];
+};
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }: any) => {
+  if (graphQLErrors) {
+    const errorType = 'graphQLError';
+    const group = formatMessage(errorType, operation);
+
+    console.groupCollapsed(...group);
+
+    graphQLErrors.map(({ message, path }: any) => {
+      const error = formatError(message, path);
+      console.log(...error);
+      return { message, path };
+    });
+
+    // TODO: Check if removing argument breaks intended functionality.
+    // @ts-ignore - Expects 0 arguments.
+    console.groupEnd(...group);
+  }
+
+  if (networkError) {
+    const errorType = 'networkError';
+    const group = formatMessage(errorType, operation);
+
+    console.groupCollapsed(...group);
+
+    const error = formatError(networkError.message);
+    console.log(...error);
+
+    // TODO: Check if removing argument breaks intended functionality.
+    // @ts-ignore - Expects 0 arguments.
+    console.groupEnd(...group);
+  }
+});
+
+const loggerLink = new ApolloLink(
+  (operation: Operation, forward?: NextLink): any => {
+    const startTime = new Date().getTime();
+
+    return (
+      forward &&
+      forward(operation).map((result) => {
+        // @ts-ignore - TS infers FragmentDefinitionNode instead of OperationDefinitionNode
+        const operationType = operation.query.definitions[0].operation;
+        const elapsed = new Date().getTime() - startTime;
+
+        const group = formatMessage(operationType, operation, elapsed);
+        console.groupCollapsed(...group);
+
+        console.log('INIT', operation);
+        console.log('RESULT', result);
+
+        // TODO: Check if removing argument breaks intended functionality.
+        // @ts-ignore - Expects 0 arguments.
+        console.groupEnd(...group);
+
+        return result;
+      })
+    );
+  },
+);
+
+export { errorLink, loggerLink };
