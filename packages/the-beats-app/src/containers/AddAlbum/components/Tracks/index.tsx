aa4db93913c3de8c536f389312606a5e7420d5b6
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2020-02-17 10:13:30 +0200

    refactor(AddAlbum): use new Accordion component in Tracks step

--- packages/the-beats-app/src/containers/AddAlbum/components/Tracks/index.tsx
+++ packages/the-beats-app/src/containers/AddAlbum/components/Tracks/index.tsx
@@ -1,8 +1,36 @@
-import { Accordion, Box, Button, Flex, Input } from '@elandamor/tra-components';
+import {
+  Accordion,
+  AccordionGroup,
+  AccordionHeader,
+  AccordionPanel,
+  Box,
+  Button,
+  Flex,
+  Input,
+  Select,
+} from '@elandamor/tra-components';
 import { FieldArray, useFormikContext } from 'formik';
 import React from 'react';
 import { MinusCircle } from 'react-feather';
-import Track from '../Track';
+import { Track } from '../../../../components';
+import { useArtistsQuery } from '../../../../generated/graphql';
+
+/**
+ * Formats select artists to DB compliant object[]
+ */
+const formatArtistsForDB = (artists: any[]) => {
+  if (!artists) return [];
+
+  const dbCompliantArtists =
+    (artists.length > 0 &&
+      artists.map((artist) => ({
+        name: artist.label,
+        ...(!artist.__isNew__ && { id: artist.value }),
+      }))) ||
+    [];
+
+  return dbCompliantArtists;
+};
 
 /**
  * @render react
@@ -15,59 +43,73 @@ import Track from '../Track';
 const Tracks = () => {
   const { values } = useFormikContext();
 
+  const { data: artistsData } = useArtistsQuery();
+  const artists = artistsData?.artists?.edges.map(({ node }: any) => ({
+    label: node.name,
+    value: node.id,
+  }));
+
   return (
-    <Box>
+    <Box marginBottom={2}>
       <FieldArray
         name="tracks"
         render={({ push, remove }) => (
-          <Box overflow="hidden">
-            {values.tracks.map((track: any, index: number) => {
-              return (
-                <Accordion
-                  key={`tracks.${index}`}
-                  headerContent={
-                    <Flex alignItems="center" marginBottom={1} width="100%">
-                      <Track
-                        data={{
-                          id: `id_${index}`,
-                          name: track.name,
-                          artists: track.artist
-                            .split(',')
-                            .filter(
-                              (artist: string) => artist.trim().length > 0,
-                            ),
-                          featuring: track.featuring
-                            .split(',')
-                            .filter(
-                              (artist: string) => artist.trim().length > 0,
-                            ),
-                          trackNumber: index + 1,
-                        }}
-                      />
-                      <Flex onClick={() => remove(index)} paddingLeft={2}>
-                        <MinusCircle />
-                      </Flex>
-                    </Flex>
-                  }
-                >
-                  <Box>
-                    <Input name={`tracks.${index}.name`} label="Title" />
-                    <Input name={`tracks.${index}.artist`} label="Artist" />
-                    <Input
-                      name={`tracks.${index}.featuring`}
-                      label="Featuring"
+          <React.Fragment>
+            <AccordionGroup>
+              {values.tracks.map((track: any, index: number) => (
+                <Accordion key={`tracks.${index}`}>
+                  <AccordionHeader>
+                    <Track
+                      hideTrackNumber={false}
+                      data={{
+                        id: `id_${index}`,
+                        name: track.name,
+                        artists: formatArtistsForDB(track.artists),
+                        featuring: formatArtistsForDB(track.featuring),
+                        trackNumber: index + 1,
+                      }}
                     />
+                  </AccordionHeader>
+                  <Box
+                    height="56px"
+                    justifyContent="center"
+                    position="absolute"
+                    right={0}
+                    top={0}
+                  >
+                    <Flex onClick={() => remove(index)} paddingLeft={2}>
+                      <MinusCircle />
+                    </Flex>
                   </Box>
+                  <AccordionPanel>
+                    <Box>
+                      <Input name={`tracks.${index}.name`} label="Title" />
+                      <Select
+                        isMulti
+                        label="Artists"
+                        name={`tracks.${index}.artists`}
+                        options={artists}
+                      />
+                      <Select
+                        isMulti
+                        label="Featuring"
+                        name={`tracks.${index}.featuring`}
+                        options={artists}
+                      />
+                    </Box>
+                  </AccordionPanel>
                 </Accordion>
-              );
-            })}
+              ))}
+            </AccordionGroup>
             <Button
               variant="outlined"
               type="button"
-              onClick={() => push({ name: '', artist: '', featuring: '' })}
+              onClick={() =>
+                push({ name: '', artists: undefined, featuring: '' })
+              }
               text="Add Track"
             />
-          </Box>
+          </React.Fragment>
         )}
       />
     </Box>
@@ -75,3 +117,17 @@ const Tracks = () => {
 };
 
 export default Tracks;
+
+/* <Track
+  data={{
+    id: `id_${index}`,
+    name: track.name,
+    artists: track.artist
+      .split(',')
+      .filter((artist: string) => artist.trim().length > 0),
+    featuring: track.featuring
+      .split(',')
+      .filter((artist: string) => artist.trim().length > 0),
+    trackNumber: ++index,
+  }}
+/>; */
