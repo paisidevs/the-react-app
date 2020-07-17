ada27db9c889f2e38408c8d4d892ce73a4665b41
Thandolwethu Mpofu <mpofuthandolwethu@gmail.com>
2020-07-17 22:11:54 +0200

    fix(tra-apollo): correctly export links

--- packages/apollo/src/index.ts
+++ packages/apollo/src/index.ts
@@ -1,2 +1,5 @@
 export * from '@apollo/client';
+export * from '@apollo/client/link/context';
+export * from '@apollo/client/link/error';
+export * from '@apollo/client/link/retry';
 export { errorLink, loggerLink } from './utilities';
